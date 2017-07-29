18b66449095db1bcc7ffe3cf0a8b98a0ddf46224
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-29 09:33:11 -0300

    Inclusão de alerts de validação fechados automaticamente.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function(){
 	$('[data-toggle="tooltip"').tooltip();
 });
+
+var t = 5000; // Tempo em milésimos
+
+setTimeout(function() {
+    $('.alert').fadeOut(); // Faz o alert ir desaparecendo com fade
+}, t);
