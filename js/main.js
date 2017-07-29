05cf44035df864e138d2ce7426b0cfc81b1ddf1b
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-29 11:36:50 -0300

    Fixando elementos com affix.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ var t = 5000; // Tempo em milésimos
 setTimeout(function() {
     $('.alert').fadeOut(); // Faz o alert ir desaparecendo com fade
 }, t);
+
+$('#menu').affix({
+  offset: {
+    top: $(window).height()
+  }
+}); 
